4d3475dc2b4c33664401c32c4e6c443a3362b44c
pparthenhs <pparthenhs@gmail.com>
2015-02-11 12:50:56 +0200

    check is allmost ready!!!

--- src/propertyFormButtons/Checker.java
+++ src/propertyFormButtons/Checker.java
@@ -2,4 +2,39 @@ package propertyFormButtons;
 
 public class Checker {
 
+	
+	public static boolean checkString(String s ){
+		
+		 char[] chars = s.toCharArray();
+
+		    for (char c : chars) {
+		        if(!Character.isLetter(c)) {
+		            return false;
+		        }
+		    }
+
+		    return true;
+		
+		}
+	
+	public static boolean checkNumber (String s) {
+		
+		if (s.matches("[0-9]+") ){
+			return true;
+		}
+		return false;
+		
+	}
+	
+	public static void main (String arg[]){
+		
+		String s="1";
+		boolean t ;
+		
+		t=checkNumber(s);
+		System.out.print(t);
+		
+		
+		
+	}
 }
