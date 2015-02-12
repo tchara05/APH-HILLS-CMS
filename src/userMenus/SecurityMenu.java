e0fa56be9afcba875d5d65450c9c5e299cccd512
Marios Pieris <mpieri04@cs.ucy.ac.cy>
2015-02-12 23:49:27 +0200

    Some tests

--- src/userMenus/SecurityMenu.java
+++ src/userMenus/SecurityMenu.java
@@ -38,7 +38,7 @@ public class SecurityMenu extends JPanel {
 		} catch (Exception e) {
 		    // If Nimbus is not available, you can set the GUI to another look and feel.
 		}
-		
+		//testing
 		frmSecurityMenu = new JFrame();
 		frmSecurityMenu.setTitle("Security Menu");
 		frmSecurityMenu.setSize(Toolkit.getDefaultToolkit().getScreenSize());
