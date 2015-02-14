7ab767d5e14f5e5b32e1f78a9693f7f6f8c04cb2
tchara05 <Theodoros@Theodoross-MBP.in.cs.ucy.ac.cy>
2015-02-14 18:59:46 +0200

    Values

--- SQL/tableCreations/Class.sql
+++ SQL/tableCreations/Class.sql
@@ -5,3 +5,9 @@ create table Class (
 	propertyClassNo int identity(1,1) primary key,
 	className varchar (50),
 );
+
+
+insert into Class  values('Luxury Villa');
+insert into Class values ('Junior Villa');
+insert into Class values ('Apartment');
+insert into Class values ('Plot');
