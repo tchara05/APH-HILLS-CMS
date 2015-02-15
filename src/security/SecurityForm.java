15551a42a4625e9583388e3b973c0a103581f8a5
Marios Pieris <mpieri04@cs.ucy.ac.cy>
2015-02-15 21:54:13 +0200

    Changes on security forms

--- src/security/SecurityForm.java
+++ src/security/SecurityForm.java
@@ -1,12 +1,31 @@
 package security;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
 
 public class SecurityForm {
 
 	private JFrame frame;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * 
@@ -19,6 +38,7 @@ public class SecurityForm {
 				try {
 					SecurityForm window = new SecurityForm();
 					window.frame.setVisible(true);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -37,12 +57,96 @@ public class SecurityForm {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		try {
+		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+		        if ("Nimbus".equals(info.getName())) {
+		            UIManager.setLookAndFeel(info.getClassName());
+		            break;
+		        }
+		    }
+		} catch (Exception e) {
+		    // If Nimbus is not available, you can set the GUI to another look and feel.
+		}
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
+		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		frame.setContentPane(contentPane);
+		contentPane.setLayout(null);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.setBounds(346, 178, 632, 42);
+		contentPane.add(panel);
+		
+		JLabel lblNewLabel = new JLabel("Add New Key Contract");
+		lblNewLabel.setFont(new Font("Calibri", Font.BOLD, 25));
+		panel.add(lblNewLabel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.LIGHT_GRAY);
+		panel_1.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel_1.setBounds(346, 231, 444, 206);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setEnabled(false);
+		textField.setEditable(false);
+		textField.setBounds(232, 28, 109, 30);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(232, 114, 109, 30);
+		panel_1.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Key ID :");
+		lblNewLabel_1.setFont(new Font("Calibri", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(36, 30, 86, 17);
+		panel_1.add(lblNewLabel_1);
+		
+		JLabel lblPropertyId = new JLabel("Property ID :");
+		lblPropertyId.setFont(new Font("Calibri", Font.PLAIN, 14));
+		lblPropertyId.setBounds(36, 122, 79, 14);
+		panel_1.add(lblPropertyId);
+		
+		JLabel lblNewLabel_2 = new JLabel("*\u03A4his field is automatically filled");
+		lblNewLabel_2.setFont(new Font("Calibri", Font.ITALIC, 11));
+		lblNewLabel_2.setBounds(232, 69, 152, 14);
+		panel_1.add(lblNewLabel_2);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.LIGHT_GRAY);
+		panel_2.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel_2.setBounds(800, 230, 178, 206);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JButton btnAddContract = new JButton("Add Contract");
+		btnAddContract.setBounds(30, 84, 116, 23);
+		panel_2.add(btnAddContract);
+		
+		JButton btnGoBack = new JButton("Go Back");
+		btnGoBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnGoBack.setBounds(30, 118, 116, 23);
+		panel_2.add(btnGoBack);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.setBounds(30, 152, 116, 23);
+		panel_2.add(btnExit);
 		
 		
 	}
-
 }
