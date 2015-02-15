ad3da9bced924aa481f514f1a53f0fb90c73d452
Marios Pieris <mpieri04@cs.ucy.ac.cy>
2015-02-15 20:51:27 +0200

    Nothing

--- src/security/SecurityForm.java
+++ src/security/SecurityForm.java
@@ -1,33 +1,22 @@
 package security;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-import java.awt.Toolkit;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.SoftBevelBorder;
-import javax.swing.border.BevelBorder;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.border.CompoundBorder;
-import javax.swing.UIManager;
-import javax.swing.JTextField;
 
-public class SecurityForm extends JFrame {
+public class SecurityForm {
 
-	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JFrame frame;
 
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					SecurityForm securityFormFrame = new SecurityForm();
-					securityFormFrame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
-					securityFormFrame.setVisible(true);
+					SecurityForm window = new SecurityForm();
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -36,71 +25,22 @@ public class SecurityForm extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the application.
 	 */
 	public SecurityForm() {
+		initialize();
+	}
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 603, 387);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel.setBounds(133, 60, 327, 42);
-		contentPane.add(panel);
 		
-		JLabel lblNewLabel = new JLabel("Add New Key Contract");
-		lblNewLabel.setFont(new Font("Calibri", Font.BOLD, 20));
-		panel.add(lblNewLabel);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		panel_1.setBounds(133, 113, 251, 203);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
-		
-		textField = new JTextField();
-		textField.setBounds(89, 27, 109, 20);
-		panel_1.add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(89, 93, 109, 20);
-		panel_1.add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblNewLabel_1 = new JLabel("Key ID :");
-		lblNewLabel_1.setFont(new Font("Calibri", Font.PLAIN, 12));
-		lblNewLabel_1.setBounds(19, 30, 86, 17);
-		panel_1.add(lblNewLabel_1);
-		
-		JLabel lblPropertyId = new JLabel("Property ID :");
-		lblPropertyId.setFont(new Font("Calibri", Font.PLAIN, 12));
-		lblPropertyId.setBounds(19, 97, 79, 14);
-		panel_1.add(lblPropertyId);
-		
-		JLabel lblNewLabel_2 = new JLabel("*\u03A4his field is automatically filled");
-		lblNewLabel_2.setFont(new Font("Calibri", Font.ITALIC, 10));
-		lblNewLabel_2.setBounds(89, 52, 152, 14);
-		panel_1.add(lblNewLabel_2);
-		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBorder(UIManager.getBorder("InternalFrame.border"));
-		panel_2.setBounds(394, 113, 66, 203);
-		contentPane.add(panel_2);
-<<<<<<< HEAD
-=======
-		
-kjkjbkjbk
-
-
-
-
->>>>>>> 62717d917f4540497148175e4a3562618f2d965a
-
 		
 	}
+
 }
