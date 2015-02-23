abeb6ef4601be8efabd9b4a9f155650931433ccf
Marios Pieris <mpieri04@cs.ucy.ac.cy>
2015-02-23 17:29:42 +0200

    Security form changes

--- src/security/SecurityForm.java
+++ src/security/SecurityForm.java
@@ -1,8 +1,12 @@
 package security;
 
 
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -16,6 +20,11 @@ import javax.swing.JButton;
 import java.awt.Color;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.JEditorPane;
+import javax.swing.JSpinner;
+import javax.swing.JScrollPane;
+import javax.swing.JComboBox;
+import javax.swing.JTextArea;
 
 public class SecurityForm {
 
@@ -68,8 +77,8 @@ public class SecurityForm {
 		    // If Nimbus is not available, you can set the GUI to another look and feel.
 		}
 		
+		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
 		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -80,22 +89,28 @@ public class SecurityForm {
 		contentPane.setLayout(null);
 		contentPane.setLayout(null);
 		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0)), "Add New Key Contract", TitledBorder.LEFT, TitledBorder.TOP, null, null));
+		panel_3.setBounds(357, 143, 635, 447);
+		contentPane.add(panel_3);
+		panel_3.setLayout(null);
+		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(Color.LIGHT_GRAY);
-		panel_1.setBorder(new LineBorder(new Color(0, 0, 0), 0));
-		panel_1.setBounds(346, 231, 368, 206);
-		contentPane.add(panel_1);
+		panel_1.setBounds(30, 34, 371, 384);
+		panel_3.add(panel_1);
+		panel_1.setBackground(new Color(204, 204, 255));
+		panel_1.setBorder(new LineBorder(new Color(0, 0, 128)));
 		panel_1.setLayout(null);
 		
 		txtkeyID = new JTextField();
 		txtkeyID.setEnabled(false);
 		txtkeyID.setEditable(false);
-		txtkeyID.setBounds(132, 33, 143, 30);
+		txtkeyID.setBounds(132, 33, 199, 30);
 		panel_1.add(txtkeyID);
 		txtkeyID.setColumns(10);
 		
 		txtpropertyID = new JTextField();
-		txtpropertyID.setBounds(132, 139, 143, 30);
+		txtpropertyID.setBounds(132, 114, 199, 30);
 		panel_1.add(txtpropertyID);
 		txtpropertyID.setColumns(10);
 		
@@ -106,7 +121,7 @@ public class SecurityForm {
 		
 		JLabel propertyLabel = new JLabel("Property ID :");
 		propertyLabel.setFont(new Font("Calibri", Font.PLAIN, 14));
-		propertyLabel.setBounds(36, 147, 79, 14);
+		propertyLabel.setBounds(36, 122, 79, 14);
 		panel_1.add(propertyLabel);
 		
 		JLabel lblNewLabel_2 = new JLabel("*\u03A4his field is automatically filled");
@@ -114,29 +129,42 @@ public class SecurityForm {
 		lblNewLabel_2.setBounds(132, 69, 152, 14);
 		panel_1.add(lblNewLabel_2);
 		
+		JLabel lblPop = new JLabel("Customer Pop up Notes :");
+		lblPop.setFont(new Font("Calibri", Font.PLAIN, 14));
+		lblPop.setBounds(36, 198, 168, 14);
+		panel_1.add(lblPop);		
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(36, 223, 295, 127);
+		panel_1.add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
+		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(Color.LIGHT_GRAY);
-		panel_2.setBorder(new LineBorder(new Color(0, 0, 0), 0));
-		panel_2.setBounds(724, 231, 178, 206);
-		contentPane.add(panel_2);
+		panel_2.setBounds(427, 34, 178, 384);
+		panel_3.add(panel_2);
+		panel_2.setBackground(new Color(204, 204, 255));
+		panel_2.setBorder(new LineBorder(new Color(0, 0, 102)));
 		panel_2.setLayout(null);
 		
 		JButton saveButton = new JButton("Add Contract");
-		saveButton.setBounds(30, 65, 116, 31);
+		saveButton.setBounds(32, 238, 116, 31);
 		panel_2.add(saveButton);
 		
 		JButton goBackButton = new JButton("Go Back");
-		goBackButton.setBounds(30, 149, 116, 31);
+		goBackButton.setBounds(32, 322, 116, 31);
 		panel_2.add(goBackButton);
 		
 		JButton clearButton = new JButton("Clear");
-		clearButton.setBounds(30, 107, 116, 31);
+		clearButton.setBounds(32, 280, 116, 31);
 		panel_2.add(clearButton);
 		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 0)), "Add New Key Contract", TitledBorder.LEFT, TitledBorder.TOP, null, null));
-		panel_3.setBounds(306, 188, 635, 287);
-		contentPane.add(panel_3);
+		ImageIcon image = new ImageIcon("Aphrodite-Hills-wp.png");
+		
+		JLabel lblNewLabel = new JLabel(image);
+		lblNewLabel.setBounds(10, 57, 158, 120);
+		panel_2.add(lblNewLabel);
 		
 	}
 	
