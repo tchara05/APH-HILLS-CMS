d642316d258f14559c2085d543eab30199d213ea
pparthenhs <pparthenhs@gmail.com>
2015-03-04 18:29:50 +0200

    kai oi allh elenxei eginan

--- src/security/CheckInSearchInfo.java
+++ src/security/CheckInSearchInfo.java
@@ -5,9 +5,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
 import javax.swing.JOptionPane;
+
 import security.CheckIn;
 import extras.DatabaseConnection;
+import extras.Checker;
+import extras.Messages;
 
 public class CheckInSearchInfo extends Thread {
 
@@ -17,7 +21,30 @@ public class CheckInSearchInfo extends Thread {
 		String keyID = CheckIn.txtSearch.getText();
 		System.out.println(keyID);
 		String specificKey = CheckIn.txtSpecificKey.getText();
-
+		boolean  flag=true;
+		
+		if(keyID.compareTo("")!=0 &&(specificKey.compareTo("A")==0 || specificKey.compareTo("B")==0||specificKey.compareTo("C")==0)){
+			try {
+				if(!checkerexist(keyID,specificKey)){
+					Messages.showWarningMessage("Not exist this compinasion on Database!");
+					flag=false;
+				}
+				else{
+					if(!checkerfields(keyID,specificKey)){
+						Messages.showWarningMessage("Not emprty fields!");
+						flag=false;
+					}
+				}
+			} catch (SQLException e) {
+				
+			}
+			
+		}
+		else{
+			flag=false;
+			Messages.showWarningMessage("Fields has error input!");
+		}
+		
 		try {
 
 			DatabaseConnection database = new DatabaseConnection();
@@ -32,7 +59,7 @@ public class CheckInSearchInfo extends Thread {
 
 			//if (response == JOptionPane.YES_OPTION) {
 
-
+			if(flag){
 				// insert query here
 				rst = stment.executeQuery("SELECT * FROM SERVICE WHERE keyID = '"
 								+ keyID + "' and specificKey = '" + specificKey + "'");
@@ -44,7 +71,7 @@ public class CheckInSearchInfo extends Thread {
 				CheckIn.txtDate.setText(rst.getString("checkOutDate"));
 
 			//}
-
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (NullPointerException e) {
@@ -52,4 +79,71 @@ public class CheckInSearchInfo extends Thread {
 			e.printStackTrace();
 		}
 	}
+	
+private boolean checkerexist(String s,String g) throws SQLException {
+		
+		if(Checker.checkNumber(s)){
+			
+			int id=Integer.parseInt(s);
+			try{
+				DatabaseConnection database = new DatabaseConnection();
+				Statement st = database.getStatement();
+				ResultSet rst = null;
+				
+				
+				rst = st.executeQuery("SELECT * FROM Service WHERE keyID='" + id+ "' and specificKey='" + g +"'");
+				
+		
+		
+					if (rst.next()){
+						return true;
+					}
+					
+			
+				
+			} catch (NullPointerException e) {
+
+				e.printStackTrace();
+			}
+		
+		}
+		return false;
+
+	}
+
+
+private boolean checkerfields(String s,String g) throws SQLException {
+	
+	
+		int id=Integer.parseInt(s);
+		try{
+			DatabaseConnection database = new DatabaseConnection();
+			Statement st = database.getStatement();
+			ResultSet rst = null;
+			
+			
+			rst = st.executeQuery("SELECT * FROM Service WHERE keyID='" + id+ "' and specificKey='" + g +"'");
+			//System.out.print("fdsa");
+			rst.next();
+			
+			String l1=rst.getString("checkInTime");
+			String l2=rst.getString("checkInDate");
+			System.out.println(l1);
+			System.out.println(l2);
+			
+			if(l1!=null && l2!=null){
+				return true;
+			}
+				
+		
+			
+		} catch (NullPointerException e) {
+
+			e.printStackTrace();
+		}
+	
+	
+	return false;
+
+}
 }
--- src/security/CheckOutButton.java
+++ src/security/CheckOutButton.java
@@ -1,14 +1,19 @@
 package security;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
 import javax.swing.JOptionPane;
+
 import security.CheckOut;
 import extras.DatabaseConnection;
+import extras.Messages;
 import userMenus.LogIn;
 import security.CheckOutClearButton;
+import extras.Checker;
 
 public class CheckOutButton extends Thread {
 
@@ -30,6 +35,25 @@ public class CheckOutButton extends Thread {
 		String checkInTime = "Key is not checked in yet";
 		String checkInDate = "Key is not checked in yet";
 
+		boolean flag=true;
+		//
+		pickUpPerson=Checker.clearString(pickUpPerson);
+		if(!Checker.checkString(pickUpPerson)){
+			Messages.showWarningMessage("PickUp Person not found");
+		}
+		
+		
+		if(keyID.compareTo("") != 0){
+			try {
+				if (!checker(keyID)){
+					Messages.showWarningMessage("Key ID not found ");
+					flag=false;
+				}
+			} catch (SQLException e1) {
+				System.out.print("2222");
+			}
+		}
+		System.out.print(flag);
 		try {
 
 			DatabaseConnection database = new DatabaseConnection();
@@ -70,4 +94,35 @@ public class CheckOutButton extends Thread {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	private boolean checker(String s) throws SQLException {
+		
+		if(Checker.checkNumber(s)){
+			
+			int id=Integer.parseInt(s);
+			try{
+				DatabaseConnection database = new DatabaseConnection();
+				Statement st = database.getStatement();
+				ResultSet rst = null;
+				
+				
+				rst = st.executeQuery("SELECT keyID FROM KeyContract WHERE keyID='" + id+"'");
+						
+					if (rst.next()){
+						return true;
+					}
+			
+				
+			} catch (NullPointerException e) {
+
+				e.printStackTrace();
+			}
+		
+		}
+		return false;
+
+	}
+	
+	
 }
