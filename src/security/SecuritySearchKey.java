53692fd1b1b99d3caa1c2a543480448f46eba631
pparthenhs <pparthenhs@gmail.com>
2015-03-16 21:08:07 +0200

    changes

--- src/security/SecuritySearchKey.java
+++ src/security/SecuritySearchKey.java
@@ -3,6 +3,9 @@ package security;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
 import extras.DatabaseConnection;
 
 public class SecuritySearchKey extends Thread {
@@ -10,28 +13,34 @@ public class SecuritySearchKey extends Thread {
 	public void run() {
 
 		String keyID = Securitymenu.txtSearchKey.getText();
-
-		try {
-
-			DatabaseConnection database = new DatabaseConnection();
-			Statement stment = database.getStatement();
-			ResultSet rst = null;
-
-			stment = database.getStatement();
-
-			// insert query here
-			rst = stment.executeQuery("SELECT * FROM KeyContract WHERE keyID = '"
-					+ keyID +  "'");
-			
-			if(rst.next())
-				Securitymenu.txtShowIfFound.setText("Key Found: " + rst.getString("keyID"));
-			else
-				Securitymenu.txtShowIfFound.setText("Key not Found");
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} catch (NullPointerException e) {
-			e.printStackTrace();
+		
+		if (keyID==null){
+			JOptionPane.showMessageDialog(null, "Non KeyID",
+					"Warning Message",
+					JOptionPane.WARNING_MESSAGE);
+		}else{
+			try {
+	
+				DatabaseConnection database = new DatabaseConnection();
+				Statement stment = database.getStatement();
+				ResultSet rst = null;
+	
+				stment = database.getStatement();
+	
+				// insert query here
+				rst = stment.executeQuery("SELECT * FROM KeyContract WHERE keyID = '"
+						+ keyID +  "'");
+				
+				if(rst.next())
+					Securitymenu.txtShowIfFound.setText("Key Found: " + rst.getString("keyID"));
+				else
+					Securitymenu.txtShowIfFound.setText("Key not Found");
+	
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
