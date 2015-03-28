bb9de4828e284ed74756201d585a413b2ee1d4af
Marios Pieris <mpieri04@cs.ucy.ac.cy>
2015-03-28 19:00:35 +0200

    Export as an excel file from database

--- src/extras/fileExcel.java
+++ src/extras/fileExcel.java
@@ -1,17 +1,71 @@
 package extras;
 
-public class fileExcel {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
-	public static void initial() {
-	Workbook wb = new HSSFWorkbook();
-	Sheet personSheet = wb.createSheet("PersonList");
-	Row headerRow = personSheet.createRow(0);
-	Cell nameHeaderCell = headerRow.createCell(0);
-	Cell addressHeaderCell = headerRow.createCell(1);
-	}
+public class fileExcel {
 	
 	public static void exportToExcel () {
 		
+		HSSFWorkbook wb = new HSSFWorkbook();
+		HSSFSheet excelSheet = wb.createSheet("Test excel sheet");
+		
+		
+		HSSFRow headerRow = excelSheet.createRow(0);
+		HSSFCell nameHeaderCell = headerRow.createCell((short) 0);
+		HSSFCell addressHeaderCell = headerRow.createCell((short) 1);
+		nameHeaderCell.setCellValue("KEY ID");
+		addressHeaderCell.setCellValue("PROPERTY ID");
+		
+		//////////////////////////////////////////////////////////
+		DatabaseConnection database = new DatabaseConnection();
+		Statement stment = null;
+		stment = database.getStatement();
+
+		String query = "SELECT * FROM KeyContract";
+		
+		int row = 1;
+
+		try {
+			ResultSet rset = stment.executeQuery(query);
+			
+			while (rset.next()) {
+				
+				String name = rset.getString("keyId");
+				String address = rset.getString("propertyId");
+
+				HSSFRow dataRow = excelSheet.createRow(row);
+
+				HSSFCell dataKeyIDCell = dataRow.createCell((short) 0);
+				dataKeyIDCell.setCellValue(name);
+
+				HSSFCell dataPropertyIDCell = dataRow.createCell((short) 1);
+				dataPropertyIDCell.setCellValue(address);
+
+				row = row + 1;
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		String outputDirPath = "D:/excelShett.xls";
+		
+		FileOutputStream fileOut;
+		try {
+			fileOut = new FileOutputStream(outputDirPath);
+			wb.write(fileOut);
+			fileOut.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
