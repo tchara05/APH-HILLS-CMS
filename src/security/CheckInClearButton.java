264bb361d4fa16c5836061f576a2b48f900173d2
pparthenhs <pparthenhs@gmail.com>
2015-03-30 18:38:20 +0300

    allages kai beltiwseis sto CheckIn searchInfo .Epishs prosarmostike to kokkino perigramma se periptwsh la8ous eisodou

--- src/security/CheckInClearButton.java
+++ src/security/CheckInClearButton.java
@@ -1,5 +1,6 @@
 package security;
 
+import javax.swing.border.Border;
 import security.CheckIn;
 
 public class CheckInClearButton {
@@ -8,8 +9,14 @@ public class CheckInClearButton {
 
 		String value = "";
 		
+		Border correct = CheckIn.txtPerson.getBorder();
+		
 		CheckIn.txtSearch.setText("key ID");
+		CheckIn.txtSearch.setBorder(correct);
+		
 		CheckIn.txtSpecificKey.setText("specific key (A,B,C)");
+		CheckIn.txtSpecificKey.setBorder(correct);
+		
 		CheckIn.txtPerson.setText(value);
 		CheckIn.txtTime.setText(value);
 		CheckIn.txtDate.setText(value);
--- src/security/CheckInSearchInfo.java
+++ src/security/CheckInSearchInfo.java
@@ -1,12 +1,15 @@
 package security;
 
+import java.awt.Color;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
-import javax.swing.JOptionPane;
+
+import javax.swing.BorderFactory;
+
+import javax.swing.border.Border;
+
 
 import security.CheckIn;
 import extras.DatabaseConnection;
@@ -18,24 +21,24 @@ public class CheckInSearchInfo extends Thread {
 	@SuppressWarnings("static-access")
 	public void run() {
 
-		String keyID = CheckIn.txtSearch.getText();
-		System.out.println(keyID);
-		String specificKey = CheckIn.txtSpecificKey.getText();
-		boolean  flag=true;
+		Border compound = null;
+		Border redline = BorderFactory.createLineBorder(Color.red,2);
 		
-		if(keyID.compareTo("")!=0 &&(specificKey.compareTo("A")==0 || specificKey.compareTo("B")==0||specificKey.compareTo("C")==0)){
+		String keyID = CheckIn.txtSearch.getText();
+		String specificKey = CheckIn.txtSpecificKey.getText();
+		boolean  flag=false;
+		
+		if(Checker.checkNumber(keyID) &&(specificKey.compareTo("A")==0 || specificKey.compareTo("B")==0||specificKey.compareTo("C")==0)){
 			try {
-				if(!checkerexist(keyID,specificKey)){
-					Messages.showWarningMessage("Not exist this compinasion on Database!");
-					new CheckInClearButton().start();
-					flag=false;
+				
+				if(checkerexist(keyID,specificKey)){
+					flag=true;
 				}
 				else{
-					if(!checkerfields(keyID,specificKey)){
-						Messages.showWarningMessage("Not emprty fields!");
-						new CheckInClearButton().start();
-						flag=false;
-					}
+				
+					Messages.showWarningMessage("Compinasion not correct OR key is checked in !");
+					new CheckInClearButton().start();
+					flag=false;
 				}
 			} catch (SQLException e) {
 				
@@ -43,10 +46,25 @@ public class CheckInSearchInfo extends Thread {
 			
 		}
 		else{
-			flag=false;
+			
+				if(!Checker.checkNumber(keyID)){
+					compound = BorderFactory.createCompoundBorder(redline, compound);
+					CheckIn.txtSearch.setBorder(compound);
+					compound = null;
+					flag=false;
+				}
+				
+				if(!(specificKey.compareTo("A")==0 || specificKey.compareTo("B")==0||specificKey.compareTo("C")==0)){
+					compound = BorderFactory.createCompoundBorder(redline, compound);
+					CheckIn.txtSpecificKey.setBorder(compound);
+					compound = null;
+					flag=false;
+				}
 			Messages.showWarningMessage("Fields has error input!");
 			new CheckInClearButton().start();
+				
 		}
+		
 		
 		try {
 
@@ -95,17 +113,12 @@ private boolean checkerexist(String s,String g) throws SQLException {
 				ResultSet rst = null;
 				String str = "Key is not checked in yet";
 				
-				rst = st.executeQuery("SELECT * FROM Service WHERE keyID='" + id+ "' and specificKey='" + g +"'" + "' and checkInTime = '" + str + "'");
-				
-				
-		
-		
-					if (rst.next()){
-						return true;
-					}
-					
+				rst = st.executeQuery("SELECT * FROM Service WHERE keyID='" + id+ "' and specificKey='" + g + "' and checkInTime = '" + str + "'");	
 			
-				
+				if (rst.next()){
+						return true;
+				}
+					
 			} catch (NullPointerException e) {
 
 				e.printStackTrace();
@@ -115,40 +128,40 @@ private boolean checkerexist(String s,String g) throws SQLException {
 		return false;
 
 	}
-
-
-private boolean checkerfields(String s,String g) throws SQLException {
-	
-	
-		int id=Integer.parseInt(s);
-		try{
-			DatabaseConnection database = new DatabaseConnection();
-			Statement st = database.getStatement();
-			ResultSet rst = null;
-			
-			
-			rst = st.executeQuery("SELECT * FROM Service WHERE keyID='" + id+ "' and specificKey='" + g +"'");
-			//System.out.print("fdsa");
-			rst.next();
-			
-			String l1=rst.getString("checkInTime");
-			String l2=rst.getString("checkInDate");
-			System.out.println(l1);
-			System.out.println(l2);
-			
-			if(l1!=null && l2!=null){
-				return true;
-			}
-				
-		
-			
-		} catch (NullPointerException e) {
-
-			e.printStackTrace();
-		}
-	
-	
-	return false;
-
 }
-}
+
+//private boolean checkerfields(String s,String g) throws SQLException {
+//	
+//	if(Checker.checkNumber(s)){
+//		int id=Integer.parseInt(s);
+//		try{
+//			DatabaseConnection database = new DatabaseConnection();
+//			Statement st = database.getStatement();
+//			ResultSet rst = null;
+//			
+//			
+//			rst = st.executeQuery("SELECT * FROM Service WHERE keyID='" + id+ "' and specificKey='" + g +"'");
+//			//System.out.print("fdsa");
+//			rst.next();
+//			
+//			String l1=rst.getString("checkInTime");
+//			String l2=rst.getString("checkInDate");
+//			System.out.println(l1);
+//			System.out.println(l2);
+//			
+//			if(l1!=null && l2!=null){
+//				return true;
+//			}
+//				
+//		
+//			
+//		} catch (NullPointerException e) {
+//
+//			e.printStackTrace();
+//		}
+//	
+//	}
+//	return false;
+//
+//}
+//}
