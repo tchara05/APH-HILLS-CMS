93d00fe5f76cf96d726cf497304484f7406b3b9e
Theodoros <tchara05@cs.ucy.ac.cy>
2015-03-31 12:56:39 +0300

    Billing Things

--- src/customer/CustomerMenu.java
+++ src/customer/CustomerMenu.java
@@ -18,6 +18,7 @@ import java.awt.event.ItemListener;
 import java.awt.event.ItemEvent;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
+import javax.swing.border.TitledBorder;
 
 public class CustomerMenu{
 
@@ -42,12 +43,14 @@ public class CustomerMenu{
 	private static JButton btnDeleteCustomer;
 	private static JButton btnEditCustomer;
 	private static JPanel detailsPanel;
+	private JPanel panel;
 
 	CustomerMenu() {
 
 		customerform = new CustomerForm();
 		CustomerForm.setVisible(false);
 		CustomerPanel = new JPanel();
+		CustomerPanel.setBackground(new Color(255, 255, 255));
 		CustomerPanel.setBorder(new LineBorder(new Color(0, 0, 128), 3));
 
 		// Labels //
@@ -72,68 +75,75 @@ public class CustomerMenu{
 		CustomerPanel.add(btnEditCustomer);
 		CustomerPanel.add(btnDeleteCustomer);
 		CustomerPanel.add(btnAddNewCustomer);
-
-		detailsPanel = new JPanel();
-		detailsPanel.setBorder(new LineBorder(new Color(0, 0, 128)));
-		detailsPanel.setBounds(20, 212, 615, 138);
-		CustomerPanel.add(detailsPanel);
-		JLabel lblNewLabel = new JLabel("Primary Email:");
-		lblNewLabel.setBounds(314, 45, 89, 16);
-		detailsPanel.setBackground(new Color(173, 216, 230));
-		detailsPanel.setLayout(null);
-		detailsPanel.add(lblNewLabel);
-
-		// Text Box //
-		PrimaryMail = new JTextField();
-		PrimaryMail.setBounds(415, 39, 194, 28);
-		detailsPanel.add(PrimaryMail);
-		PrimaryMail.setEditable(false);
-		PrimaryMail.setColumns(15);
-		JLabel lblContactPhone = new JLabel("Contact Phone:");
-		lblContactPhone.setBounds(308, 73, 95, 16);
-		detailsPanel.add(lblContactPhone);
-
-		contactNumber = new JTextField();
-		contactNumber.setBounds(415, 67, 194, 28);
-		detailsPanel.add(contactNumber);
-		contactNumber.setEditable(false);
-		contactNumber.setColumns(10);
-
-		MobilePhone = new JTextField();
-		MobilePhone.setBounds(115, 39, 181, 28);
-		detailsPanel.add(MobilePhone);
-		MobilePhone.setEditable(false);
-		MobilePhone.setColumns(15);
-		JLabel lblMobilePhone = new JLabel("Mobile Phone:");
-		lblMobilePhone.setBounds(0, 45, 88, 16);
-		detailsPanel.add(lblMobilePhone);
-		JLabel lblNewLabel_1 = new JLabel("Customer ID:");
-		lblNewLabel_1.setBounds(111, 11, 83, 16);
-		detailsPanel.add(lblNewLabel_1);
-
-		CustomerID = new JTextField();
-		CustomerID.setBounds(206, 5, 161, 28);
-		detailsPanel.add(CustomerID);
-		CustomerID.setEditable(false);
-		CustomerID.setColumns(10);
-		JLabel lblFirstName = new JLabel("First Name:");
-		lblFirstName.setBounds(0, 73, 72, 16);
-		detailsPanel.add(lblFirstName);
-
-		FirstName = new JTextField();
-		FirstName.setBounds(115, 67, 181, 28);
-		detailsPanel.add(FirstName);
-		FirstName.setEditable(false);
-		FirstName.setColumns(15);
-		JLabel lblLastname = new JLabel("Last Name:");
-		lblLastname.setBounds(0, 101, 70, 16);
-		detailsPanel.add(lblLastname);
-
-		LastName = new JTextField();
-		LastName.setBounds(115, 95, 181, 28);
-		detailsPanel.add(LastName);
-		LastName.setEditable(false);
-		LastName.setColumns(15);
+		
+		panel = new JPanel();
+		panel.setBorder(new TitledBorder(new LineBorder(new Color(0, 0, 128), 3), "Customer Details", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(20, 211, 617, 176);
+		CustomerPanel.add(panel);
+		panel.setLayout(null);
+		
+				detailsPanel = new JPanel();
+				detailsPanel.setBounds(16, 19, 584, 135);
+				panel.add(detailsPanel);
+				detailsPanel.setBorder(new LineBorder(new Color(0, 0, 128)));
+				JLabel lblNewLabel = new JLabel("Primary Email:");
+				lblNewLabel.setBounds(308, 45, 89, 16);
+				detailsPanel.setBackground(new Color(173, 216, 230));
+				detailsPanel.setLayout(null);
+				detailsPanel.add(lblNewLabel);
+				
+						// Text Box //
+						PrimaryMail = new JTextField();
+						PrimaryMail.setBounds(415, 39, 164, 28);
+						detailsPanel.add(PrimaryMail);
+						PrimaryMail.setEditable(false);
+						PrimaryMail.setColumns(15);
+						JLabel lblContactPhone = new JLabel("Contact Phone:");
+						lblContactPhone.setBounds(308, 73, 95, 16);
+						detailsPanel.add(lblContactPhone);
+						
+								contactNumber = new JTextField();
+								contactNumber.setBounds(415, 67, 164, 28);
+								detailsPanel.add(contactNumber);
+								contactNumber.setEditable(false);
+								contactNumber.setColumns(10);
+								
+										MobilePhone = new JTextField();
+										MobilePhone.setBounds(115, 39, 181, 28);
+										detailsPanel.add(MobilePhone);
+										MobilePhone.setEditable(false);
+										MobilePhone.setColumns(15);
+										JLabel lblMobilePhone = new JLabel("Mobile Phone:");
+										lblMobilePhone.setBounds(15, 45, 88, 16);
+										detailsPanel.add(lblMobilePhone);
+										JLabel lblNewLabel_1 = new JLabel("Customer ID:");
+										lblNewLabel_1.setBounds(111, 11, 83, 16);
+										detailsPanel.add(lblNewLabel_1);
+										
+												CustomerID = new JTextField();
+												CustomerID.setBounds(206, 5, 161, 28);
+												detailsPanel.add(CustomerID);
+												CustomerID.setEditable(false);
+												CustomerID.setColumns(10);
+												JLabel lblFirstName = new JLabel("First Name:");
+												lblFirstName.setBounds(15, 73, 72, 16);
+												detailsPanel.add(lblFirstName);
+												
+														FirstName = new JTextField();
+														FirstName.setBounds(115, 67, 181, 28);
+														detailsPanel.add(FirstName);
+														FirstName.setEditable(false);
+														FirstName.setColumns(15);
+														JLabel lblLastname = new JLabel("Last Name:");
+														lblLastname.setBounds(15, 101, 70, 16);
+														detailsPanel.add(lblLastname);
+														
+																LastName = new JTextField();
+																LastName.setBounds(115, 95, 181, 28);
+																detailsPanel.add(LastName);
+																LastName.setEditable(false);
+																LastName.setColumns(15);
 		addButtonsFuctionalities();
 	}
 
